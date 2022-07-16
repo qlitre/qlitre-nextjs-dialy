55a8f7a4ddaf4b2a457e7e0eb481693276fa63b5
qlitre <kuri0403@gmail.com>
2022-07-17 01:47:39 +0900

    buildのバグを解消

--- src/components/PostList.tsx
+++ src/components/PostList.tsx
@@ -6,7 +6,6 @@ import {
     Link,
     Text,
     Button,
-    useColorModeValue,
 } from "@chakra-ui/react";
 import { Datetime } from '../components/Datetime'
 
@@ -18,41 +17,39 @@ type Props = {
 export const PostList = ({ posts }: Props) => {
     return (
         <>
-            <Stack>
-                {posts.map(post => (
-                    <>
-                        <Box>
-                            <Link href={`/post/${post.id}`}>
-                                <Heading
-                                    as="h2"
-                                    fontSize="3xl"
-                                    lineHeight={1.6}
-                                    marginTop="4"
-                                    flex={1}
-                                    cursor="pointer"
-                                >
-                                    {post.title}
-                                </Heading>
-                            </Link>
-                            <Datetime datetime={post.publishedAt} format="yyyy-MM-dd" />
-                            <Text mt="2" fontSize="xl" textColor={useColorModeValue('gray.700', "white")}>{post.description}</Text>
-                            <Link href={`/post/${post.id}`}>
-                                <Button
-                                    colorScheme='teal'
-                                    variant='outline'
-                                    size="sm"
-                                    mt="8"
-                                >
-                                    続きを読む
-                                </Button>
-                            </Link>
-                        </Box>
-                        <Box>
-                            <Stack mt="10" mb="10" borderBottom="1px" borderColor="gray.300" />
-                        </Box>
-                    </>
-                ))}
-            </Stack>
+            {posts.map(post => (
+                <>
+                    <Box key={post.id}>
+                        <Link href={`/post/${post.id}`}>
+                            <Heading
+                                as="h2"
+                                fontSize="3xl"
+                                lineHeight={1.6}
+                                marginTop="4"
+                                flex={1}
+                                cursor="pointer"
+                            >
+                                {post.title}
+                            </Heading>
+                        </Link>
+                        <Datetime datetime={post.publishedAt} format="yyyy-MM-dd" />
+                        <Text mt="2" fontSize="xl" >{post.description}</Text>
+                        <Link href={`/post/${post.id}`}>
+                            <Button
+                                colorScheme='teal'
+                                variant='outline'
+                                size="sm"
+                                mt="8"
+                            >
+                                続きを読む
+                            </Button>
+                        </Link>
+                    </Box>
+                    <Box>
+                        <Stack mt="10" mb="10" borderBottom="1px" borderColor="gray.300" />
+                    </Box>
+                </>
+            ))}
         </>
     )
 }
