d3b1226d9c125d4e63520780bcf7e2028901d6b3
qlitre <kuri0403@gmail.com>
2022-07-22 06:13:20 +0900

    Google Analytics

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,9 @@
 import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react';
+import { useGoogleAnalytics } from "libs/gtag";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useGoogleAnalytics();
   return (
     <ChakraProvider>
       <Component {...pageProps} />
