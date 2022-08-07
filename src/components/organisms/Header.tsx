85c459db4796b253f9c808559cd5689ef556b5c3
qlitre <kuri0403@gmail.com>
2022-08-07 18:49:12 +0900

    aboutページの作成

--- src/components/organisms/Header.tsx
+++ src/components/organisms/Header.tsx
@@ -1,6 +1,10 @@
 import NextLink from "next/link";
-import { MoonIcon, SunIcon } from '@chakra-ui/icons';
+import { MoonIcon, SunIcon, HamburgerIcon } from '@chakra-ui/icons';
 import {
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuList,
     Box,
     Flex,
     Button,
@@ -10,11 +14,15 @@ import {
     Heading,
     Link,
     HStack,
+    Icon,
+    IconButton,
+    useBreakpointValue
 } from '@chakra-ui/react';
 import { config } from 'settings/siteSettings';
 
 export const Header = () => {
     const { colorMode, toggleColorMode } = useColorMode();
+    const screenIsWider = useBreakpointValue([false, false, true], "sm");
     return (
         <Box bg={useColorModeValue('gray.100', 'gray.900')} px={4}>
             <Container maxW="container.lg">
@@ -25,22 +33,61 @@ export const Header = () => {
                         </Heading>
                     </NextLink>
                     <HStack as="nav" spacing="3">
-                        <Link
-                            isExternal
-                            href={config.repository}
-                            display="flex"
-                            alignItems="center"
-                            gap="2"
-                            fontWeight="bold"
-                        >
-                            GitHub
-                        </Link>
                         <Button size='lg' onClick={toggleColorMode}>
                             {colorMode === 'light' ? <MoonIcon /> : <SunIcon />}
                         </Button>
+                        {screenIsWider ? <NavLinks /> : <CollapsedNavLinks />}
                     </HStack>
                 </Flex>
             </Container>
         </Box>
     );
 }
+
+const NavLinks = () => {
+    return (
+        <>
+            <Link
+                href="/about"
+                display="flex"
+                alignItems="center"
+                gap="2"
+                fontWeight="bold"
+            >
+                About
+            </Link>
+            <Link
+                isExternal
+                href={config.repository}
+                display="flex"
+                alignItems="center"
+                gap="2"
+                fontWeight="bold"
+            >
+                GitHub
+            </Link>
+
+        </>
+    );
+};
+
+const CollapsedNavLinks = () => {
+    return (
+        <Menu>
+            <MenuButton
+                as={IconButton}
+                aria-label="ナビゲーションリンクを開閉する"
+                icon={<Icon as={HamburgerIcon} />}
+                variant="outline"
+            />
+            <MenuList>
+                <MenuItem as="a" href="/about">
+                    About
+                </MenuItem>
+                <MenuItem as="a" target="_blank" rel="noreferer" href={config.repository}>
+                    GitHub
+                </MenuItem>
+            </MenuList>
+        </Menu>
+    );
+};
