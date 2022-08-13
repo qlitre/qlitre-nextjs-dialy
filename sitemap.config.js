0a5f732bec44b4f46a49101b48522f322373aebe
qlitre <kuri0403@gmail.com>
2022-08-13 23:16:54 +0900

    sitemapを動的に生成するようにした

--- sitemap.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    siteUrl: 'https://www.qlitre-dialy.ink',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    outDir: './out',
-};
---
+++ src/pages/sitemap.xml.tsx
@@ -0,0 +1,39 @@
+import { GetServerSidePropsContext } from 'next'
+import { getAllContents } from 'libs/getAllContents'
+import { Post } from 'types/blog'
+import { config } from 'settings/siteSettings'
+
+async function generateSitemapXml() {
+    let xml = `<?xml version="1.0" encoding="UTF-8"?>`
+    xml += `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">`
+
+    const posts = await getAllContents()
+
+    posts.forEach((post: Post) => {
+        xml += `
+      <url>
+        <loc>${config.siteUrl}/post/${post.id}</loc>
+        <lastmod>${post.updatedAt}</lastmod>
+      </url>
+    `
+    })
+
+    xml += `</urlset>`
+    return xml
+}
+
+export const getServerSideProps = async ({ res, }: GetServerSidePropsContext) => {
+    const xml = await generateSitemapXml()
+
+    res.statusCode = 200
+    res.setHeader('Cache-Control', 's-maxage=86400, stale-while-revalidate')
+    res.setHeader('Content-Type', 'text/xml')
+    res.end(xml)
+
+    return {
+        props: {},
+    }
+}
+
+const Page = () => null
+export default Page
