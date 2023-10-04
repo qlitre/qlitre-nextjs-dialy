7f95a20a3ba493fd53c409ad0759b40a206bb5bc
qlitre <kuri0403@gmail.com>
2023-10-04 22:32:06 +0900

    microCMSのlimit上限対応

--- src/libs/getAllPostsContents.ts
+++ src/libs/getAllPostsContents.ts
@@ -4,6 +4,13 @@ import type { Post } from 'types/blog'
 export const getAllPostsContents = async () => {
     const data = await client.getList<Post>({ endpoint: "post", queries: { fields: 'id' } });
     const totalCount = data.totalCount;
-    const allData = await client.getList<Post>({ endpoint: "post", queries: { limit: totalCount } });
-    return allData.contents
+    const maxLimit = 50;
+    const loop = Math.ceil(totalCount / maxLimit);
+    const promises = [...Array(loop)].map((_, i) => {
+        const offset = i * maxLimit;
+        return client.getList<Post>({ endpoint: "post", queries: { limit: maxLimit, offset: offset } });
+    });
+    const results = await Promise.all(promises);
+    const contents = results.flatMap((result) => result.contents);
+    return contents
 }
--- src/libs/getCategoryList.ts
+++ src/libs/getCategoryList.ts
@@ -2,6 +2,6 @@ import { client } from 'libs/client'
 import type { PostCategory } from 'types/blog'
 
 export const getCategoryList = async () => {
-    const listData = await client.getList<PostCategory>({ endpoint: "category", queries: { limit: 100 } });
+    const listData = await client.getList<PostCategory>({ endpoint: "category", queries: { limit: 50 } });
     return listData.contents
 }
